4592cd9c3160b94f1f182a1037b271b26ba1945b
Pedro Costa <pedroc_aragao@outlook.com>
2023-11-15 14:23:01 -0300

    implement remaining activities

--- app/src/main/java/com/example/imdmarket/MainActivity.java
+++ app/src/main/java/com/example/imdmarket/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.imdmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button loginButton = findViewById(R.id.btn_login);
+
+        loginButton.setOnClickListener(view -> startActivity(new Intent(this, HomeActivity.class)));
     }
 }
